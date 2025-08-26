ea37bd3e2e2050b363aa3567f9f0e194ccea9c29
rafael <rafaelpjmp@gmail.com>
2025-08-26 17:14:23 -0300

    eliminando aquivo ser uso

--- config.py
+++
@@ -1,2 +0,0 @@
-URL_DADOS = "https://raw.githubusercontent.com/rafa3lsilva/webscrapping_redscore/refs/heads/main/dados_redscore.csv"
-URL_BASE_JOGOS = "https://raw.githubusercontent.com/rafa3lsilva/webscrapping_redscore/refs/heads/main/jogos_do_dia"
